583d0b1f7496a654cdc9ac19889580a9d31e9850
Blen Berhanu Gebretsadik <blenberhanu01@gmail.com>
2026-01-01 14:20:12 +0300

    Update API endpoint

--- frontend/script.js
+++ frontend/script.js
@@ -25,7 +25,7 @@ document
 
     try {
       const response = await fetch(
-        `http://127.0.0.1:8000/predict?model=${model}`,
+        `https://exam-pass-api-1.onrender.com/predict?model=${model}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
